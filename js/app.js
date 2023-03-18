94b4fc2311be8fea59f67a877983ed8664dbd778
reboot13-git <rautkrutik013@gmail.com>
2023-03-18 18:55:05 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,30 +1,12 @@
-let servicesInfo=[
-    {
-        "imgUrl": "https://img.icons8.com/nolan/64/web.png",
-        "heading": "Web Development",
-        "description": "Our Developers offer full fledged support for the website you imagine, brought to life. We give you top quality performance in prices so affordable you can't believe",
-        "linkText": "Get your own Website now →", 
-        "id": "web"
 
+let servicesInfo = '/js/services.json'
 
-    },
-    {
-        "imgUrl": "https://img.icons8.com/nolan/64/adobe-illustrator.png",
-        "heading": "Designing",
-        "description" : "We help you deal with talented designers whether it be graphics or UI/UX designs. Make your project stand out and power up your brand, with our effective and quality services.",
-        "linkText": "Get an awesome design →",
-        "id": "design"
+fetch(servicesInfo)
+.then(response => response.json())
+.then(data => servicesInfo = data)
+.then(() => displayCard())
 
-    },
-    {
-        "imgUrl": "https://img.icons8.com/nolan/64/pen.png",
-        "heading": "Content Writing",
-        "description" : "Content that moves people, Yes please. We make content that makes you stand out and presence known.Discuss your next project for the best engaging writings.",
-        "linkText": "Get Content Writer →",
-        "id": "cntwrt"
-
-    }
-]
+    
 
 
 let scards = document.getElementById("scards")
