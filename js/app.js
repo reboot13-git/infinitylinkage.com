1a386345893b1750578838bedb8b4815e935bc3a
reboot13-git <rautkrutik013@gmail.com>
2023-04-27 18:45:33 +0530

    update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
 
-let servicesInfo = '/js/services.json'
+// let servicesInfo = '/js/services.json'
 
-fetch(servicesInfo)
-.then(response => response.json())
-.then(data => servicesInfo = data)
-.then(() => displayCard())
-
-    
+// fetch(servicesInfo)
+// .then(response => response.json())
+// .then(data => servicesInfo = data)
+// .then(() => displayCard())
 
 
-let scards = document.getElementById("scards")
 
-function displayCard(){
-    servicesInfo.forEach(item=> {
-scards.innerHTML += `<div class="col "  data-aos="fade-up" >
-<div class="card" id=${item.id}  >
-<img src=${item.imgUrl} loading="lazy" alt=${item.heading} />
-<h4> ${item.heading} </h4>
-<p> ${item.description} </p>
-<a href="#contactus" class="nav-link p-0 m-0"><h6>${item.linkText}</h6></a>
-</div>
-</div>`
-    })
+// let scards = document.getElementById("scards")
+
+// function displayCard(){
+//     servicesInfo.forEach(item=> {
+// scards.innerHTML += `<div class="col "  data-aos="fade-up" >
+// <div class="card" id=${item.id}  >
+// <img src=${item.imgUrl} loading="lazy" alt=${item.heading} />
+// <h4> ${item.heading} </h4>
+// <p> ${item.description} </p>
+// <a href="#contactus" class="nav-link p-0 m-0"><h6>${item.linkText}</h6></a>
+// </div>
+// </div>`
+//     })
+// }
+
+
+
+window.onload = function () {
+    alert("We'are rightnow revamping our website, don't judge us by our current website.Directly writing code in production.")
 }
-
-displayCard()
